6940ecff577f01b0e54049042c94ae59f4aa09c0
Larissa Ramos <68797820+larisramos@users.noreply.github.com>
2022-02-26 00:52:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+NumFunc = int(input())
+HorasTrabalhadas = int(input())
+ValorHora = float(input())
+salario = HorasTrabalhadas * ValorHora
+print ('NUMBER = {}'.format(NumFunc)+'\nSALARY = U$ {:.2f}'.format(salario))
